d67e1b7f92cbb7dfbe7a149c2611a795d3608ffa
PolyFlower <btchead@protonmail.ch>
2022-09-18 05:11:29 +0300

    ✨feat: added crypto functionality * eac cache encryption * eac cache decryption

--- src/utils.ts
+++ src/utils.ts
@@ -108,4 +108,67 @@ export namespace Utils {
       return cdnFileBuffer;
     }
   }
+
+  export namespace Crypto {
+    /**
+     * Decrypts .eac cache file byte buffer.
+     *
+     * Essentially it's a void func. Just decrypt.
+     * @param {Buffer} [image] Encrypted image byte buffer to decrypt.
+     * @param {number} [imageSize] Length in bytes to decrypt. Original have that because of excessive memory allocations (half of image can be zeroed out)
+     */
+    export function DecryptCache(image: Buffer, imageSize: number): void {
+      /* eac_x86.dll: .text:6C1C4990
+      {
+       unsigned int v2; // eax
+       if ( imageSize >= 2 )
+       {
+         image[imageSize - 1] += 3 - 3 * imageSize;
+         v2 = imageSize - 2;
+         if ( imageSize != 2 )
+         {
+           do
+           {
+              image[v2] += -3 * v2 - image[v2 + 1];
+              --v2;
+           }
+           while ( v2 );
+          }
+          *image -= image[1];
+        }
+      } */
+      var v2: number;
+      if (imageSize >= 2) {
+        image[imageSize - 1] += 3 - 3 * imageSize;
+        v2 = imageSize - 2;
+        if (imageSize != 2) {
+          do {
+            image[v2] += -3 * v2 - image[v2 + 1];
+            --v2;
+          } while (v2);
+        }
+        image[0] -= image[1];
+      }
+    }
+
+    /**
+     * Encrypt using .eac cache encryption.
+     * @param {Buffer} [image] Image byte buffer to encrypt.
+     * @param {number} [imageSize] Length in bytes to encrypt.
+     */
+    export function EncryptCache(image: Buffer, imageSize: number): void {
+      var v2: number;
+      if (imageSize >= 2) {
+        image[imageSize - 1] += 3 - 3 * imageSize;
+        v2 = 0;
+        if (imageSize != 2) {
+          do {
+            image[v2] -= -3 * v2 - image[v2 + 1];
+            ++v2;
+          } while (v2 < imageSize);
+        }
+        // image[0] -= image[1];
+      }
+    }
+  }
 }
